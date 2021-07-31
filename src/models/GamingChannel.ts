c4569230d57f6c2ef9b04386af0fdb703703ea5e
Maxhmoyano <maxhmoyano@gmail.com>
2021-07-31 19:00:14 -0300

    Deleted name as a required field in gaming channel db

--- src/models/GamingChannel.ts
+++ src/models/GamingChannel.ts
@@ -1,12 +1,11 @@
 import { Document, model, Schema } from 'mongoose';
 
 export interface IGamingChannel extends Document {
-  name: string;
+  _id: string;
 }
 
 const GamingChannel = new Schema({
   _id: { type: String, required: [true, 'El id es obligatorio'] },
-  name: { type: String, required: [true, 'El nombre del canal creado es obligatorio'] },
 });
 
 export default model<IGamingChannel>('GamingChannel', GamingChannel);
